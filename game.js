d6314ee2f2a91668d3fc4643367f1a8bda631267
Vardan <vardan.vanyan@gmail.com>
2021-12-07 18:00:46 -0800

    made changes to the javascript code

--- game.js
+++ game.js
@@ -17,23 +17,24 @@ function nextSequence() {
   //generate a random number between 0 and 3
   //store random number in a variable
   let randomNumber = Math.floor(Math.random() * 4);
-  return randomNumber;
+  //variable that holds a random color
+  let randomChosenColor = buttonColours[randomNumber];
+  //pushing the random color to the game pattern
+  gamePattern.push(randomChosenColor);
+
+  //selecting the button with the same id as our randomly generated color
+  console.log($(`div#${randomChosenColor}`));
+  //making this element flash
+  $(`div#${randomChosenColor}`)
+    .fadeOut(100)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+
+  //playing audio depending on the chose color
+  let audio = new Audio(`sounds/${randomChosenColor}.mp3`);
+  audio.play();
 }
-
-//variable that holds a random color
-let randomChosenColor = buttonColours[nextSequence()];
-//pushing the random color to the game pattern
-gamePattern.push(randomChosenColor);
-
-//selecting the button with the same id as our randomly generated color
-console.log($(`div#${randomChosenColor}`));
-//making this element flash
-$(`div#${randomChosenColor}`).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-
-//playing audio depending on the chose color
-let audio = new Audio(`sounds/${randomChosenColor}.mp3`);
-audio.muted = false;
-audio.play();
 
 //detect which button is being clicked
 $("div.btn").on("click", function (event) {
